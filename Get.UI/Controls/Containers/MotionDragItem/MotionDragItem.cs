a8b320e7766620db6f1607061253726c0e975a15
getdn <getdna99@gmail.com>
2023-12-26 15:28:51 -0800

    Add Pointer Visual State stuff

--- Get.UI/Controls/Containers/MotionDragItem/MotionDragItem.cs
+++ Get.UI/Controls/Containers/MotionDragItem/MotionDragItem.cs
@@ -31,4 +31,54 @@ public partial class MotionDragItem : Control
         RootElement.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
         InitManipulation();
     }
+    [Property(Visibility = GeneratorVisibility.Protected, SetVisibility = GeneratorVisibility.Private, OnChanged = nameof(OnPointerVisualStateUpdated))]
+    PointerVisualState _CurrentPointerVisualState;
+    protected virtual void OnPointerVisualStateUpdated()
+    {
+        VisualStateManager.GoToState(this, CurrentPointerVisualState.ToString(), true);
+    }
+    bool isPointerInside = false;
+    bool isPressed = false;
+    protected override void OnPointerEntered(PointerRoutedEventArgs e)
+    {
+        isPointerInside = true;
+        UpdateState();
+        base.OnPointerEntered(e);
+    }
+    protected override void OnPointerExited(PointerRoutedEventArgs e)
+    {
+        isPointerInside = false;
+        UpdateState();
+        base.OnPointerExited(e);
+    }
+    protected override void OnPointerPressed(PointerRoutedEventArgs e)
+    {
+        isPressed = true;
+        UpdateState();
+        base.OnPointerPressed(e);
+    }
+    protected override void OnPointerReleased(PointerRoutedEventArgs e)
+    {
+        isPressed = false;
+        UpdateState();
+        base.OnPointerReleased(e);
+    }
+    void UpdateState()
+    {
+
+        if (isPressed) CurrentPointerVisualState = PointerVisualState.Pressed;
+        else
+        {
+            if (isPointerInside)
+                CurrentPointerVisualState = PointerVisualState.PointerOver;
+            else
+                CurrentPointerVisualState = PointerVisualState.Normal;
+        }
+    }
+    protected enum PointerVisualState
+    {
+        Normal,
+        PointerOver,
+        Pressed
+    }
 }
